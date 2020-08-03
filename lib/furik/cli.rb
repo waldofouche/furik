c4a2661fe4029e0430b97ca5d8a69b8ea17f5b86
Christopher Jones <chris.jones120@outlook.com>
2020-08-03 12:02:05 +1000

    Remove PR functionality
    It is outdated and slow, and isn't required for the Activity log which
    is primarily what I want to use.

--- lib/furik/cli.rb
+++ lib/furik/cli.rb
@@ -3,40 +3,6 @@ require 'thor'
 
 module Furik
   class Cli < Thor
-    desc 'pulls', 'show pull requests'
-    method_option :start_date, type: :string, aliases: '-s'
-    method_option :end_date, type: :string, aliases: '-e'
-    def pulls
-      start_date = Date.parse(options[:start_date]) if options[:start_date]
-      end_date = Date.parse(options[:end_date]) if options[:end_date]
-
-      puts "Pull Requests#{" (#{start_date}...#{end_date})" if start_date && end_date}"
-      puts '-'
-      puts ''
-
-      Furik.pull_requests do |repo, issues|
-        if issues && !issues.empty?
-          string_issues = issues.each.with_object('') do |issue, memo|
-            date = issue.created_at.localtime.to_date
-
-            next if start_date && date < start_date
-            next if end_date && date > end_date
-
-            memo << "- [#{issue.title}](#{issue.html_url}):"
-            memo << " (#{issue.body.plain.cut})" if issue.body && !issue.body.empty?
-            memo << " #{issue.created_at.localtime.to_date}\n"
-          end
-
-          unless string_issues == ''
-            puts "### #{repo}"
-            puts ''
-            puts string_issues
-            puts ''
-          end
-        end
-      end
-    end
-
     desc 'activity', 'show activity'
     method_option :since, type: :numeric, aliases: '-d', default: 0
     method_option :from, type: :string, aliases: '-f', default: Date.today.to_s
